43aed79c011252422918e49c85530cdd3777a86b
Sreenig1980 <sreenig1980@gmail.com>
2023-11-13 05:21:21 +0530

    one more time

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,35 +1,38 @@
 # import functions from Airflow 
 
+from airflow.operators.postgres_operator import PostgresOperator
 from airflow import DAG
-from datetime import datetime
-import pandas as pd
-import json
+from datetime import datetime, date
 import requests
-from airflow.operators.bash_Operator import bashOperator
-from airflow.operators.python import pythonOperator
+import json
+import pandas as pd
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
 
 # setup default arguments 
 
 default_args = {
-  'author':'sreenivasulu',
-  'description':'this is my first Dag craeation with simple code',
-  'start_date':'datetime(2023,11,01),
-  'schedule_interval':@daily
+  'author': 'sreenivasulu',
+  'description': 'this is my first Dag craeation with simple code',
+  'start_date' : datetime(2023,11,01),
+  'schedule_interval': @daily
 }
 
 # define function
 def print_function(name):
-  print("my name is + name ")
+  print("my name is" + name)
 
 # DAG setup
 
 dag1 = DAG(
-  'dag_id' = 'sample_dag',
-  'default_args' = default_args,
-  'author' = 'sreenivasulu',
-  'description' = 'this is my first DAG creation',
-  'schedule_interval'='0 9 * * *',
-  'catchup' = False,
+  dag_id = 'sample_dag',
+  default_args = default_args,
+  author = 'sreenivasulu',
+  description = 'this is my first DAG creation',
+  schedule_interval='0 9 * * *',
+  catchup = False,
 )
 
 # Task setup
